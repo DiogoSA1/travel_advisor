acf9173b0cf8a32768e39c71a9e04874e0a7400b
Diogo Albuquerque <albuquerquediogo@hotmail.com>
2022-01-25 17:43:24 -0300

    Implantaçao de componentes e API

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import { CssBaseline, Grid } from '@material-ui/core';
 
+import { getPlacesData } from './api';
 import Header from './components/Header/Header';
 import List from './components/List/List';
 import Map from './components/Map/Map';
+import { LocationCityRounded } from '@material-ui/icons';
 
 const App = () => {
+    const [places, setplaces] = useState([]);
+
+    const [coordinates, setCoordinates] = useState({});
+    const [bounds, setBounds] = useState(null);
+
+    useEffect(() => {
+        navigator.geolocation.getCurrentPosition(({coords: {latitude, longitude} }) => {
+            setCoordinates({lat: LocationCityRounded, lng: longitude});
+        })
+    }, []);
+    
+    useEffect(() => {
+        getPlacesData()
+            .then((data) => {
+                console.log(data);
+                setplaces(data);
+            })
+    }, [coordinates, bounds]);
+
+
+
     return (
         <>
             <CssBaseline />
@@ -15,8 +38,12 @@ const App = () => {
                 <Grid item xs={12} md={4} >
                     <List />
                 </Grid>
-                <Grid item xs={12} md={4} >
-                    <Map />
+                <Grid item xs={12} md={8} >
+                    <Map 
+                        setCoordinates={setCoordinates}
+                        setBounds={setBounds}
+                        coordinates={coordinates}
+                    />
                 </Grid>
             </Grid>
         </>
