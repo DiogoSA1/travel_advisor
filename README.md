ee796605bff1375b0eecb4dfdcd6e7788dcfd292
dalbuquerque <dalbuquerque@hsolmkt.com.br>
2022-01-26 13:55:17 -0300

    finalizando filtro de restaurantes

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 "# travel_advisor" 
 
+yarn install
+
 npx create-react-app ./
 
 yarn add @material-ui/core @material-ui/icons @material-ui/lab @react-google-maps/api axios google-map-react  
--- src/App.js
+++ src/App.js
@@ -9,24 +9,29 @@ import Map from './components/Map/Map';
 
 const App = () => {
     const [places, setPlaces] = useState([]);
+    const [childClicked, setChildClicked] = useState(null);
 
     const [coordinates, setCoordinates] = useState({});
     const [bounds, setBounds] = useState({});
 
-    useEffect(() => {
+    const [isLoading, setIsLoading] = useState(false);
+
+    useEffect(() => {        
         navigator.geolocation.getCurrentPosition(({coords: {latitude, longitude} }) => {
             setCoordinates({lat: latitude, lng: longitude});
         })
     }, []);
 
     useEffect(() => {
-        getPlacesData(bounds.sw, bounds.ne)
-            .then((data) => {                
-                setPlaces(data);
-            })
+        setIsLoading(true);
+        if (bounds && bounds.sw && bounds.ne) {
+            return getPlacesData(bounds.sw, bounds.ne)
+                .then((data) => {                
+                    setPlaces(data);
+                    setIsLoading(false);
+                })
+        } 
     }, [coordinates, bounds]);
-
-
 
     return (
         <>
@@ -34,13 +39,19 @@ const App = () => {
             <Header />
             <Grid container spacing={3} style={{ width: '100%'}}>
                 <Grid item xs={12} md={4} >
-                    <List places={places} />
+                    <List 
+                        places={places} 
+                        childClicked={childClicked}
+                        isLoading={isLoading}
+                    />
                 </Grid>
                 <Grid item xs={12} md={8} >
                     <Map 
                         setCoordinates={setCoordinates}
                         setBounds={setBounds}
                         coordinates={coordinates}
+                        setChildClicked={setChildClicked}
+                        places={places}
                     />
                 </Grid>
             </Grid>
